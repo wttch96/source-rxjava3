c43ba4be8717647e6a588b88ec6d7fbc2c6b1696
wttch <wttch@wttch.com>
2020-12-16 17:59:12 +0800

    core.BackpressureOverflowStrategy;

--- src/main/java/io/reactivex/rxjava3/core/BackpressureOverflowStrategy.java
+++ src/main/java/io/reactivex/rxjava3/core/BackpressureOverflowStrategy.java
@@ -1,27 +1,14 @@
-/**
- * Copyright (c) 2016-present, RxJava Contributors.
- *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
- *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.reactivex.rxjava3.core;
 
-/** Options to deal with buffer overflow when using onBackpressureBuffer. */
+/** 使用onBackpressureBuffer时处理缓冲区溢出的选项. */
 public enum BackpressureOverflowStrategy {
   /**
-   * Signal a {@link io.reactivex.rxjava3.exceptions.MissingBackpressureException
-   * MissingBackpressureException} and terminate the sequence.
+   * 发出{@link io.reactivex.rxjava3.exceptions.MissingBackpressureException
+   * MissingBackpressureException}信号并终止序列.
    */
   ERROR,
-  /** Drop the oldest value from the buffer. */
+  /** 从缓冲区中删除最早的值. */
   DROP_OLDEST,
-  /** Drop the latest value from the buffer. */
+  /** 从缓冲区中删除最新值. */
   DROP_LATEST
 }
