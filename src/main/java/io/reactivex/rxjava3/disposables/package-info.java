51a7d755574045bd9f83d31710151e2db0e2c21e
wttch <wttch@wttch.com>
2020-12-16 17:51:18 +0800

    disposables;

--- src/main/java/io/reactivex/rxjava3/disposables/package-info.java
+++ src/main/java/io/reactivex/rxjava3/disposables/package-info.java
@@ -1,20 +1,6 @@
 /**
- * Copyright (c) 2016-present, RxJava Contributors.
- *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
- *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * Default implementations for {@link io.reactivex.rxjava3.disposables.Disposable Disposable}-based
- * resource management ({@code Disposable} container types) and utility classes to construct {@link
- * io.reactivex.rxjava3.disposables.Disposable Disposables} from callbacks and other types.
+ * 基于{@link io.reactivex.rxjava3.disposables.Disposable Disposable}的资源管理({@code
+ * Disposable}容器类型)和实用程序类的默认实现, 用于构造{@link io.reactivex.rxjava3.disposables.Disposable
+ * Disposables}和回调.
  */
 package io.reactivex.rxjava3.disposables;
