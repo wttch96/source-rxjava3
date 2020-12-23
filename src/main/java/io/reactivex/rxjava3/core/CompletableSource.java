acc3571e006be1091f55b6c6f7e1b0a7b6bf1462
wttch <wttch@wttch.com>
2020-12-23 14:30:14 +0800

    core.CompletableSource;

--- src/main/java/io/reactivex/rxjava3/core/CompletableSource.java
+++ src/main/java/io/reactivex/rxjava3/core/CompletableSource.java
@@ -1,23 +1,9 @@
-/**
- * Copyright (c) 2016-present, RxJava Contributors.
- *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
- *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.reactivex.rxjava3.core;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 
 /**
- * Represents a basic {@link Completable} source base interface, consumable via an {@link
- * CompletableObserver}.
+ * 表示基本的{@link Completable}源基本接口, 可通过{@link CompletableObserver}进行使用.
  *
  * @since 2.0
  */
@@ -25,10 +11,10 @@ import io.reactivex.rxjava3.annotations.NonNull;
 public interface CompletableSource {
 
   /**
-   * Subscribes the given {@link CompletableObserver} to this {@code CompletableSource} instance.
+   * 将给定的{@link CompletableObserver}订阅到此{@code CompletableSource}实例.
    *
-   * @param observer the {@code CompletableObserver}, not {@code null}
-   * @throws NullPointerException if {@code observer} is {@code null}
+   * @param observer {@code CompletableObserver}, 而不是{@code null}
+   * @throws NullPointerException 如果{@code observer}为{@code null}
    */
   void subscribe(@NonNull CompletableObserver observer);
 }
