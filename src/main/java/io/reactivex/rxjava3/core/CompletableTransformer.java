da4be452c4812f6a6f1264082b86f626db96a507
wttch <wttch@wttch.com>
2020-12-23 16:31:48 +0800

    core.CompletableTransformer;

--- src/main/java/io/reactivex/rxjava3/core/CompletableTransformer.java
+++ src/main/java/io/reactivex/rxjava3/core/CompletableTransformer.java
@@ -1,31 +1,15 @@
-/**
- * Copyright (c) 2016-present, RxJava Contributors.
- *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
- *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.reactivex.rxjava3.core;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 
-/**
- * Convenience interface and callback used by the compose operator to turn a {@link Completable}
- * into another {@code Completable} fluently.
- */
+/** 组合运算符使用的便捷接口和回调将{@link Completable}流畅地转换为另一个{@code Completable}. */
 @FunctionalInterface
 public interface CompletableTransformer {
   /**
-   * Applies a function to the upstream {@link Completable} and returns a {@link CompletableSource}.
+   * 将函数应用于上游{@link Completable}并返回{@link CompletableSource}.
    *
-   * @param upstream the upstream {@code Completable} instance
-   * @return the transformed {@code CompletableSource} instance
+   * @param upstream 上游{@code Completable}实例
+   * @return 转换后的{@code CompletableSource}实例
    */
   @NonNull
   CompletableSource apply(@NonNull Completable upstream);
