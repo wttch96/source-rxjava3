84c6a598c14efbafca8885f571a30f6cba3095a4
wttch <wttch@wttch.com>
2020-12-23 14:52:20 +0800

    core.CompletableConverter;

--- src/main/java/io/reactivex/rxjava3/core/CompletableConverter.java
+++ src/main/java/io/reactivex/rxjava3/core/CompletableConverter.java
@@ -1,23 +1,9 @@
-/**
- * Copyright (c) 2016-present, RxJava Contributors.
- *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
- *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.reactivex.rxjava3.core;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 
 /**
- * Convenience interface and callback used by the {@link Completable#to} operator to turn a
- * Completable into another value fluently.
+ * {@link Completable＃to}运算符使用的便利接口和回调函数, 可以将Completable流畅地转换为另一个值.
  *
  * <p>History: 2.1.7 - experimental
  *
@@ -27,10 +13,10 @@ import io.reactivex.rxjava3.annotations.NonNull;
 @FunctionalInterface
 public interface CompletableConverter<@NonNull R> {
   /**
-   * Applies a function to the upstream Completable and returns a converted value of type {@code R}.
+   * 将函数应用于上游Completable并返回类型为{@code R}的转换值.
    *
-   * @param upstream the upstream Completable instance
-   * @return the converted value
+   * @param upstream 上游Completable实例
+   * @return 转换后的值
    */
   R apply(@NonNull Completable upstream);
 }
